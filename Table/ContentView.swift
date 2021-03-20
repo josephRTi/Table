92ce9c2cefe1e3051a86ed7cc71b3c5507d6b3fb
josephrt <josephrti@gmail.com>
2021-03-20 10:01:35 +0300

    Initial Commit

---
+++ Table/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Table
+//
+//  Created by Егор Уваров on 20.03.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
